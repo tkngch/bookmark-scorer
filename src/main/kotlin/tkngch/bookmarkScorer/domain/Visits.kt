c99095431a700c63e1a328a6a73a211772c19084
Takao Noguchi <tkngch@runbox.com>
2020-12-29 11:40:11 +0000

    Rename the variable for clarity

--- src/main/kotlin/tkngch/bookmarkScorer/domain/Visits.kt
+++ src/main/kotlin/tkngch/bookmarkScorer/domain/Visits.kt
@@ -12,13 +12,13 @@ data class Visits(val records: List<Visit>) {
 data class Visit(val bookmarkId: String, val visitDates: List<Instant>) {
     fun inferAverageDailyVisits(): Double {
         val now = Instant.now()
-        val validVisitDates = visitDates.filter { it < now }
-        val sinceDate: Instant = validVisitDates.sorted().firstOrNull() ?: now
+        val pastVisitDates = visitDates.filter { it < now }
+        val sinceDate: Instant = pastVisitDates.sorted().firstOrNull() ?: now
 
         // ChronoUnit.DAYS.between gives the number of days (upper bound exclusive). Plus 1 to make it inclusive.
         val nDatesBetweenSinceDateAndToday = ChronoUnit.DAYS.between(getDate(sinceDate), getDate(now)) + 1
 
-        return validVisitDates.size / nDatesBetweenSinceDateAndToday.toDouble()
+        return pastVisitDates.size / nDatesBetweenSinceDateAndToday.toDouble()
     }
 
     private fun getDate(x: Instant) = LocalDate.ofInstant(x, ZoneOffset.UTC)
